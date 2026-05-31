12f00b5f7595a198c24e185ef9ed9d0bb220d815
VaniaHarchenko <Vaniaharchenko76@gmail.com>
2026-05-31 14:12:31 +0300

    [1] Commit
    - Реалізовано завдання 2
    - Реалізовано завдання 3

--- EducationalProcessModeling/EducationalProcess.cs
+++ EducationalProcessModeling/EducationalProcess.cs
@@ -8,22 +8,30 @@ namespace EducationalProcessModeling
 {
     public class EducationalProcess
     {
-        // Списки для збереження об'єктів моделювання
         public List<EducationForm> EducationForms { get; set; } = new List<EducationForm>();
         public List<Lesson> Lessons { get; set; } = new List<Lesson>();
         public List<Student> Students { get; set; } = new List<Student>();
         public List<Teacher> Teachers { get; set; } = new List<Teacher>();
 
-        // Поля для Завдань 4, 10
         public string MethodologicalSupport { get; set; } = "Стандартне забезпечення (Реліз 1)";
         public double QualityScore { get; set; } = 100.0;
 
         public EducationalProcess()
         {
-            // Ініціалізація даних для ЗАВДАННЯ 1 (Реліз 1)
+            // [Завдання 1]
             EducationForms.Add(new EducationForm("Денна", 9, "Максимум взаємодії з викладачами, регулярні заняття."));
             EducationForms.Add(new EducationForm("Заочна", 2, "Самостійне навчання з короткими сесіями вичитки лекцій."));
             EducationForms.Add(new EducationForm("Дистанційна", 9, "Навчання через онлайн-платформи в синхронному/асинхронному режимі."));
+
+            // [Завдання 2] Ініціалізація занять та видів контролю
+            Lessons.Add(new Lesson("Вища математика (Лекція)", "Залік"));
+            Lessons.Add(new Lesson("Об'єктно-орієнтоване програмування (Практика)", "Іспит"));
+            Lessons.Add(new Lesson("Фізика (Лабораторна)", "Залік"));
+
+            // [Завдання 3] Планування (задаємо час у розкладі для цих занять)
+            Lessons[0].ScheduleTime = "Понеділок, 08:30";
+            Lessons[1].ScheduleTime = "Вівторок, 10:15";
+            Lessons[2].ScheduleTime = "Середа, 12:00";
         }
 
         // Метод для виводу Завдання 1
@@ -34,6 +42,29 @@ namespace EducationalProcessModeling
             {
                 form.DisplayInfo();
             }
+        }
+
+        // Метод для виводу Завдання 2
+        public void ShowLessonsAndControl()
+        {
+            Console.WriteLine("=== [ЗАВДАННЯ 2] Види навчальних занять і контролю ===");
+            foreach (var lesson in Lessons)
+            {
+                Console.WriteLine($"• Заняття: {lesson.Title} | Форма контролю: {lesson.ControlType}");
+            }
+            Console.WriteLine();
+        }
+
+        // Метод для виводу Завдання 3
+        public void ShowEducationPlanning()
+        {
+            Console.WriteLine("=== [ЗАВДАННЯ 3] Планування освітнього процесу (Розклад) ===");
+            foreach (var lesson in Lessons)
+            {
+                // 
+                Console.WriteLine($"[План] {lesson.ScheduleTime} -> {lesson.Title}");
+            }
+            Console.WriteLine();
         }
     }
 }
